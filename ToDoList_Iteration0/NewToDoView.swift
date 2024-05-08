ce56c2d0ae8e3760c22f3d5656cc83d8d2a93180
Marnie Reid <marnie@kodewithklossy.com>
2024-05-08 16:15:15 -0400

    Complete Iteration 0 & ToDoItem Class

---
+++ ToDoList_Iteration0/NewToDoView.swift
@@ -0,0 +1,18 @@
+//
+//  NewToDoView.swift
+//  ToDoList_Iteration0
+//
+//  Created by scholar on 5/8/24.
+//
+
+import SwiftUI
+
+struct NewToDoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewToDoView()
+}
---
+++ ToDoList_Iteration0/ToDoItem.swift
@@ -0,0 +1,8 @@
+//
+//  ToDoItem.swift
+//  ToDoList_Iteration0
+//
+//  Created by scholar on 5/8/24.
+//
+
+import Foundation
